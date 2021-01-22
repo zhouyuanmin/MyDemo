14e28d4d59a5b2ca085c39de55889b549dd107b8
Yuanmin Zhou <1837722596@qq.com>
2021-01-22 14:14:05 +0800

    请求头参数的区别使用headers = {Content-Type: application/json}

--- 2021-01/demo21-1.py
+++ 2021-01/demo21-1.py
@@ -3,41 +3,24 @@ import json
 from pprint import pprint
 
 url = "http://httpbin.org/post"
-params = {'access_token': "1234567890"}
+params = {'access_token': "access_token"}
 headers = {'Content-Type': 'application/json'}
-data = {'text': "text", 'index': "index"}
+data = {123: 456}
 
-# 注意观察data的值到底去了哪里 data = {'text': "text", 'index': "index"}
-res1 = requests.post(url=url, headers=headers, params=params, data=json.dumps(data)).json()
+# 注意观察headers不同的时候，data的值到底去了哪里 data = {123: 456}
+res1 = requests.post(url=url, headers=headers, data=json.dumps(data)).json()
 
-res2 = requests.post(url=url, headers=headers, params=params, data=data).json()
+res2 = requests.post(url=url, headers=headers, data=data).json()
 
-res3 = requests.post(url=url, params=params, data=data).json()
-
-# print(res1)
-# print(res2)
-# print(res3)
-
-# 去除headers参数
-res1.pop('headers')
-res2.pop('headers')
-res3.pop('headers')
-
-# 去除origin参数
-res1.pop('origin')
-res2.pop('origin')
-res3.pop('origin')
-
-# 去除url参数
-res1.pop('url')
-res2.pop('url')
-res3.pop('url')
-
-# 去除files参数
-res1.pop('files')
-res2.pop('files')
-res3.pop('files')
+res3 = requests.post(url=url, data=data).json()
 
 pprint(res1)
 pprint(res2)
 pprint(res3)
+
+"""
+总结
+添加 contentType：“application/json“之后，向后台发送数据的格式必须为json字符串，
+不添加 contentType：“application/json“的时候可以向后台发送json对象形式
+json字符串和json对象的区别
+"""
